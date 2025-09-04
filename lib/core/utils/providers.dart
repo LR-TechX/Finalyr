12bdff49b716306f8321744eeb35b74a4d1e9ec8
Scott <114173655+LR-TechX@users.noreply.github.com>
2025-09-04 05:16:52 +0100

    Update providers.dart

--- lib/core/utils/providers.dart
+++ lib/core/utils/providers.dart
@@ -11,62 +11,22 @@ import '../services/memory_service.dart';
 import '../services/tips_service.dart';
 import '../services/wifi_scanner_service.dart';
 
-final settingsProvider =
-    StateNotifierProvider<SettingsController, AppSettings>((ref) {
-  return SettingsController();
+final settingsProvider = StateProvider<AppSettings>((ref) => AppSettings.defaults);
+
+final chatHistoryProvider = StateNotifierProvider<ChatHistoryNotifier, List<ChatMessage>>(
+  (ref) => ChatHistoryNotifier(),
+);
+
+final chatServiceProvider = Provider<ChatService>((ref) {
+  return ChatService(
+    knowledgeBase: KnowledgeBaseService.instance,
+    memory: MemoryService.instance,
+  );
 });
 
-class SettingsController extends StateNotifier<AppSettings> {
-  SettingsController() : super(AppSettings.defaults) {
-    _load();
-  }
+class ChatHistoryNotifier extends StateNotifier<List<ChatMessage>> {
+  ChatHistoryNotifier() : super(const []);
 
-  Future<void> _load() async {
-    final box = await Hive.openBox<String>('settings_box');
-    final raw = box.get('settings');
-    if (raw != null) {
-      state = AppSettings.fromJson(jsonDecode(raw) as Map<String, dynamic>);
-    }
-    await KnowledgeBaseService.instance.load();
-    await MemoryService.instance.init();
-    await TipsService.instance.load();
-    await TipsService.instance.scheduleDailyTip();
-  }
-
-  Future<void> update(AppSettings newState) async {
-    state = newState;
-    final box = Hive.box<String>('settings_box');
-    await box.put('settings', jsonEncode(state.toJson()));
-  }
+  void add(ChatMessage msg) => state = [...state, msg];
+  void clear() => state = const [];
 }
-
-final chatProvider =
-    StateNotifierProvider<ChatController, List<ChatMessage>>((ref) {
-  return ChatController(ref);
-});
-
-class ChatController extends StateNotifier<List<ChatMessage>> {
-  final Ref ref;
-
-  ChatController(this.ref) : super(const []);
-
-  Future<void> send(String message) async {
-    final id = const Uuid().v4();
-    final user = ChatMessage.user(id, message);
-    state = [...state, user];
-
-    final settings = ref.read(settingsProvider);
-    final reply =
-        await ChatService.instance.getReply(message: message, settings: settings);
-    final bot = ChatMessage.bot(const Uuid().v4(), reply);
-    state = [...state, bot];
-  }
-}
-
-final wifiScannerProvider = FutureProvider<WifiScannerResult>((ref) async {
-  return WifiScannerService.instance.scan();
-});
-
-final tipsProvider = Provider<String>((ref) {
-  return TipsService.instance.randomTip();
-});
