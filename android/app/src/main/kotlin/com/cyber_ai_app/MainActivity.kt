0b5db17c95c25886fdb725c42ca28a931b627956
Scott <114173655+LR-TechX@users.noreply.github.com>
2025-09-04 05:04:11 +0100

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/cyber_ai_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/cyber_ai_app/MainActivity.kt
@@ -1,6 +1,5 @@
-// File: android/app/src/main/kotlin/com/cyber_ai_app/MainActivity.kt
 package com.cyber_ai_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
